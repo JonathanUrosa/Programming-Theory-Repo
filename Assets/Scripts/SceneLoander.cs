093497e97de7fa9d2085d836fc87c6a57dd934aa
JonathanUrosa <51730896+JonathanUrosa@users.noreply.github.com>
2022-11-26 10:22:29 -0400

    Title Screen
    - Added text Title
    - Added Scene Loader

---
+++ Assets/Scripts/SceneLoander.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoander : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        StartCoroutine(DelaySceneLoander());
+    }
+    IEnumerator DelaySceneLoander()
+    {
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(1);
+    }
+}
